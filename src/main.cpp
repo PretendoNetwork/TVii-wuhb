d29fdd2cd528ec0380862eafb2b1555c513b5883
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-02-17 13:11:01 -0500

    Removed useless Mocha

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <coreinit/debug.h>
 #include <whb/proc.h>
-#include <mocha/mocha.h>
 #include <sysapp/title.h>
 #include <sysapp/launch.h>
 
@@ -8,21 +7,14 @@ int main() {
 	WHBProcInit();
 
 	if (WHBProcIsRunning()) {
-		if (Mocha_InitLibrary() == MOCHA_RESULT_SUCCESS) {
-			if (
-				SYSCheckTitleExists(0x000500301001300A) || // * JPN
-				SYSCheckTitleExists(0x000500301001310A) || // * USA
-				SYSCheckTitleExists(0x000500301001320A)    // * EUR
-			) {
-				_SYSSwitchTo(SYSAPP_PFID_TVII);
-			} else {
-				OSFatal("Failed to find TVii");
-			}
-
-			Mocha_DeInitLibrary();
-
+		if (
+			SYSCheckTitleExists(0x000500301001300A) || // * JPN
+			SYSCheckTitleExists(0x000500301001310A) || // * USA
+			SYSCheckTitleExists(0x000500301001320A)    // * EUR
+		) {
+			_SYSSwitchTo(SYSAPP_PFID_TVII);
 		} else {
-			SYSLaunchMenu();
+			OSFatal("Failed to find TVii");
 		}
 
 		while(WHBProcIsRunning()) {}
