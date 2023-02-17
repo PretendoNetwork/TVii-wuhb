8f8c3e615593c74e8cb00b0db2da4af1b3d6fe69
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-02-17 09:22:21 -0500

    Initial push

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <coreinit/debug.h>
+#include <whb/proc.h>
+#include <mocha/mocha.h>
+#include <sysapp/title.h>
+#include <sysapp/launch.h>
+
+int main() {
+	WHBProcInit();
+
+	if (WHBProcIsRunning()) {
+		if (Mocha_InitLibrary() == MOCHA_RESULT_SUCCESS) {
+			if (SYSCheckTitleExists(0x000500301001300A)) { // * JPN
+				_SYSSwitchTo(SYSAPP_PFID_TVII);
+			} else if(SYSCheckTitleExists(0x000500301001310A)) { // * USA
+				_SYSSwitchTo(SYSAPP_PFID_TVII);
+			} else if(SYSCheckTitleExists(0x000500301001320A)) { // * EUR
+				_SYSSwitchTo(SYSAPP_PFID_TVII);
+			} else {
+				OSFatal("Failed to find TVii");
+			}
+
+			Mocha_DeInitLibrary();
+
+		} else {
+			SYSLaunchMenu();
+		}
+
+		while(WHBProcIsRunning()) {}
+	}
+
+	return 0;
+}
